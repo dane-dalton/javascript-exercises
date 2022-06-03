a8f7dccd2e02febc5fba1c9175f1677d7fd9f8d8
Dane Dalton <danedalton@sbcglobal.net>
2022-06-03 16:42:08 -0500

    Ex. 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reverse = ''
+    let newRvrsChar = ''
+    let length = str.length
 
+    for(let i = length - 1; i >= 0; i--) {
+        newRvrsChar = str.charAt(i)
+        reverse += newRvrsChar
+    }
+
+    return reverse
 };
 
 // Do not edit below this line
