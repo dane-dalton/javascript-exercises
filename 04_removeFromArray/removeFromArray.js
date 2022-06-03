7ab4fce4cd423f4cb624763ca8452b2cdf0bfb49
Dane Dalton <danedalton@sbcglobal.net>
2022-06-03 17:14:23 -0500

    Ex. 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, e1, e2, e3, e4) {
+    let length = array.length
+    let newArr = []
 
+    for ( let i = 0; i < length; i++ ) {
+        if ( array[i] === e1 || array[i] === e2 || array[i] === e3 || array[i] === e4 ) {
+            continue
+        }
+        newArr.push(array[i])
+    }
+
+    return newArr
 };
 
 // Do not edit below this line
