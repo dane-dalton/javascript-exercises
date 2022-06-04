ba97ce9c9da3ebc88e15591caa31de9cbf339840
Dane Dalton <danedalton@sbcglobal.net>
2022-06-04 09:32:48 -0500

    Ex. 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let cel = (num - 32) * (5/9)
+  cel = Math.round(cel * 10) / 10
+  return cel
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let far = (num * (9/5)) + 32
+  far = Math.round(far * 10) / 10
+  return far
 };
 
 // Do not edit below this line
