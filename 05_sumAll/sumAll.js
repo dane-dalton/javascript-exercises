e73f93b3f585cda5045dd9f52a809a9d984f3769
Dane Dalton <danedalton@sbcglobal.net>
2022-06-04 09:07:03 -0500

    Ex. 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+    
+    if (!(typeof num1 === 'number') || !(typeof num2 === 'number')) {
+        return 'ERROR'
+    }
 
+    let sum = 0
+    let min
+    let max
+
+    if (num1 >= num2) {
+        min = num2
+        max = num1
+    } else {
+        min = num1
+        max = num2
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
