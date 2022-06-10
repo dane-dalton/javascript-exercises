4907d3989ecc700a5357c3bebd19cfd0b0c678dd
Dane Dalton <danedalton@sbcglobal.net>
2022-06-10 16:16:33 -0500

    Ex. 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let numInt = parseInt(num)
 
+    if (numInt <= 0) return 'OOPS'
+    if (numInt == 1 || numInt == 2) return 1
+
+    const firstNum = 1
+    const secondNum = 1
+
+    let previous = firstNum
+    let current = secondNum
+    let temp
+
+    for(let i = 2; i < numInt; i++) {
+        temp = current
+        current = current + previous
+        previous = temp
+    }
+
+    return current
 };
 
 // Do not edit below this line
