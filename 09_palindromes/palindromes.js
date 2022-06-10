1a412fe32a2794ea5fe2642ca97f5d1632b5c914
Dane Dalton <danedalton@sbcglobal.net>
2022-06-10 15:49:38 -0500

    Ex. 9 complete.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //lowercase string
+    const lowerStr = str.toLowerCase()
 
+    //spread the string into an array
+    const strArray = [...lowerStr]
+
+    //filters characters that arent letters
+    const filteredArr = strArray.filter(char => char.match(/[a-z]/))
+
+    //create an empty array to store reversed string
+    let reversedArr = []
+
+    //declare reverse counter
+    let rvrsCount = filteredArr.length - 1
+
+    //store reversed string in array and check if beginning = end
+    for (let i = 0; i < filteredArr.length; i++) {
+        
+        reversedArr.push(filteredArr[rvrsCount])
+        rvrsCount--
+        if (!(filteredArr[i] === reversedArr[i])) {
+            return false
+        }
+    }
+
+    return true
 };
 
 // Do not edit below this line
