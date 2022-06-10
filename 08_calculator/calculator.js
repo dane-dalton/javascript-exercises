f9170ce39b5ebc8bdfa6f50915a9c2cff6957ac0
Dane Dalton <danedalton@sbcglobal.net>
2022-06-10 14:35:36 -0500

    Ex. 8 complete. Emphasis on .reduce() for combining multiple values in an array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+	const sum = values.reduce((total, value) => {
+    return total + value
+  }, 0)
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(values) {
+  const multiplyAll = values.reduce((total, value) => {
+    return total * value
+  }, 1)
 
+  return multiplyAll
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNum) {
+  let factorialCountDown = factorialNum
+  let array = []
+
+  if (factorialNum == 0 || factorialNum == 1) return 1
+
+  while(factorialCountDown > 1) {
+    array.push(factorialCountDown)
+    factorialCountDown--
+  }
+
+  const final = array.reduce((total, value) => {
+    return total * value
+  }, 1)
+
+  return final
 };
 
 // Do not edit below this line
