8e9159fcd1850817f4f3c9b35a6da054748f5605
Dane Dalton <danedalton@sbcglobal.net>
2022-06-10 18:59:30 -0500

    Ex. 12 complete with two solutions. One is using reduce and the other is using sort

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((old, person) => {
+        const { name, yearOfBirth, yearOfDeath } = person
+        const isEmpty = Object.keys(old).length === 0
+    
+        if(isEmpty) {
+            old['name'] = name
+            old['yearOfBirth'] = yearOfBirth
+            old['yearOfDeath'] = yearOfDeath
+        }
+    
+        const currentOldAge = (old.yearOfDeath) ? old.yearOfDeath - old.yearOfBirth : new Date().getFullYear() - old.yearOfBirth
+        const personOldAge = (yearOfDeath) ? yearOfDeath - yearOfBirth : new Date().getFullYear() - yearOfBirth
+    
+        if (personOldAge > currentOldAge) {
+            old['name'] = name
+            old['yearOfBirth'] = yearOfBirth
+            old['yearOfDeath'] = yearOfDeath
+        }
+    
+        return old
+    }, {})
 
+    // const oldest = people.sort((a, b) => {
+    //     const aAge = (a.yearOfDeath) ? a.yearOfDeath - a.yearOfBirth : new Date().getFullYear() - a.yearOfBirth
+    //     const bAge = (b.yearOfDeath) ? b.yearOfDeath - b.yearOfBirth : new Date().getFullYear() - b.yearOfBirth
+
+    //     return (aAge > bAge) ? -1 : 1
+    // })
+
+    return oldest
 };
 
 // Do not edit below this line
